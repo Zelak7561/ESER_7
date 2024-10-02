409ef9dc638975abfbf64473d69c117279aff1de
Mario21145 <mariomazziotti756@gmail.com>
2024-10-02 18:24:48 +0200

    Exercise finished

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+    const int a = 6;
+    const char b = 'A';
+    const float c = 12331222;
+    float d = 8.14245322;
+    // float e = ((c * d) * 2.0) % 10;
+
+
+    printf("%d\n", a);
+    printf("%c\n", b);
+    printf("%lf\n", c);
+    printf("%lf\n", d);
+
+
+    printf("Hello, World!\n");
+    return 0;
+}
